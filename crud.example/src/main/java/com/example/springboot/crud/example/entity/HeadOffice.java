10c3b1e7711e508fc472deea353a08703483913a
Abhikamra30 <abhineet.kamra84@gmail.com>
2023-03-13 21:24:01 +0000

    Head office added

---
+++ crud.example/src/main/java/com/example/springboot/crud/example/entity/HeadOffice.java
@@ -0,0 +1,13 @@
+package com.example.springboot.crud.example.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class HeadOffice {
+
+	private String HeadOfficeManager;
+}
